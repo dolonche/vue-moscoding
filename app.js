381432ccde06673303483aa6f7d1475174621bdd
Иван Питенин <ivan.pitenin@gmail.com>
2019-10-13 05:58:42 +0300

    complete second task

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+// Task1
 for (let i = 1; i <= 10; i++) {
 	console.log(i);
 }
+
+// Task2
+const cleanPhoneNumber = phone => phone.replace(/-/g, '')
+console.log(cleanPhoneNumber('8-966-544'));
+
+const anythingToLowerCase = anything => {
+	if (!anything) {
+		return ''
+	}
+	return String(anything).toLowerCase()
+}
+console.log(anythingToLowerCase('SLKSLSKL'));
